72174321e90367420891447c257da48bb5f5dc94
Suhag Al Amin <developersuhag@gmail.com>
2021-10-24 21:49:28 +0600

    get post

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
@@ -16,7 +21,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("conntect to db");
+    const database = client.db("carMehanics");
+    const servicesCollection = database.collection("service");
+
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // GET a single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
+    // POST API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await servicesCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
   } finally {
     // await client.close()
   }
